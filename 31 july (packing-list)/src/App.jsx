45f0e9451b8cdbd2242debf21e3da3fb3ce1b244
abhi-001001 <abhishekgulawala81@gmail.com>
2025-08-09 18:11:35 +0530

    assigment

---
+++ 31 july (packing-list)/src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import PackingList from './packinglist.jsx';
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'Arial, sans-serif', padding: '20px' }}>
+      <h1>🎒 Packing List</h1>
+      <PackingList />
+    </div>
+  );
+}
+
+export default App;
